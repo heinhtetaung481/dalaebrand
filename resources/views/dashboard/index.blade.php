53055f8fa30ab62aaa5103eb4540be6e3c1412b1
ClassicalHHA <heinhtetaung481@gmail.com>
2018-05-13 19:53:59 +0630

    fixed conflicts again

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -7,10 +7,8 @@
 		<div class="panel panel-default card" id="pending-orders-card">
 			<div class="panel-body">
 
-				<span>{{ $pendingOrdersCount }}</span> <br>Pending Orders
-
 				<div class="col-md-8">
-					<span class="numbers">24</span> <br>Pending Orders
+					<span class="numbers">{{ $pendingOrdersCount }}</span> <br>Pending Orders
 				</div>
 				<div class="col-md-4">
 					<span class="glyphicon glyphicon-shopping-cart card-icon"></span>
@@ -22,10 +20,8 @@
 		<div class="panel panel-default card" id="products-card">
 			<div class="panel-body">
 
-				<span>{{ $itemsCount }}</span> <br>Products
-
 				<div class="col-md-8">
-					<span class="numbers">4</span> <br>Products
+					<span class="numbers">{{ $itemsCount }}</span> <br>Products
 				</div>
 				<div class="col-md-4">
 					<span class="glyphicon glyphicon-list-alt card-icon"></span>
@@ -39,11 +35,9 @@
 
 			<div class="panel-body">
 
-				<span>{{ $salesCount }}</span> <br>Sales
-
 
 				<div class="col-md-8">
-					<span class="numbers">24</span> <br>Sales
+					<span class="numbers">{{ $salesCount }}</span> <br>Sales
 				</div>
 
 				<div class="col-md-4">
@@ -59,10 +53,8 @@
 		<div class="panel panel-default card" id="customers-card">
 			<div class="panel-body">
 
-				<span>{{ $customersCount }}</span> <br>Customers
-
 				<div class="col-md-8">
-					<span class="numbers">2402</span> <br>Customers
+					<span class="numbers">{{ $customersCount }}</span> <br>Customers
 				</div>
 				<div class="col-md-4">
 					<span class="glyphicon glyphicon-user card-icon"></span>
@@ -73,35 +65,29 @@
 	</div>
 </div>
 
-<div class="row">
+<div>
 	<div class="col-md-6">
-		<div class="panel panel-default panel-danger">
-			<div class="panel-heading">STOCKS UNDER 10 PAX</div>
+
+		<div class="panel panel-default panel-primary">
+			<div class="panel-heading">Popular Items <span class="glyphicon glyphicon-star pull-right"><span></div>
 			<div class="panel-body">
 				<table class="table table-striped">
-						<tr>
-							<th>Type</th>
-							<th>Color</th>
-							<th>Size</th>
-							<th>Qty</th>
-						</tr>
-						@if($leastItems->count() > 0)
-							@foreach($leastItems as $item)
-								<tr>
-									<td>{{ $item->itemtype->gender }} {{ $item->itemtype->type }}</td>
-									<td>{{ $item->color }}</td>
-									<td>{{ $item->size }}</td>
-									<td>{{ $item->quantity }}</td>
-								</tr>
-							@endforeach
-						@else
-							<tr><td colspan="4">All Items are above 10 Pax</td></tr>
-						@endif
-
-						
+					<tr>
+						<th>Type</th>
+						<th>Color</th>
+						<th>Size</th>
+						<th>Total Sales</th>
+					</tr>
+					<tr>
+						<td>Male Slipper</td>
+						<td>White</td>
+						<td>11</td>
+						<td>480</td>
+					</tr>
 				</table>
 			</div>
 		</div>
+		
 	</div>
 
 	<div class="col-md-6">
@@ -132,32 +118,34 @@
 		</div>
 	</div>
 
-</div>
-
-<div class="row">
 	<div class="col-md-6">
-		<div class="panel panel-default panel-primary">
-			<div class="panel-heading">Popular Items <span class="glyphicon glyphicon-star pull-right"><span></div>
+		<div class="panel panel-default panel-danger">
+			<div class="panel-heading">STOCKS UNDER 10 PAX</div>
 			<div class="panel-body">
 				<table class="table table-striped">
-					<tr>
-						<th>Type</th>
-						<th>Color</th>
-						<th>Size</th>
-						<th>Total Sales</th>
-					</tr>
-					<tr>
-						<td>Male Slipper</td>
-						<td>White</td>
-						<td>11</td>
-						<td>480</td>
-					</tr>
+						<tr>
+							<th>Type</th>
+							<th>Color</th>
+							<th>Size</th>
+							<th>Qty</th>
+						</tr>
+						@if($leastItems->count() > 0)
+							@foreach($leastItems as $item)
+								<tr>
+									<td>{{ $item->itemtype->gender }} {{ $item->itemtype->type }}</td>
+									<td>{{ $item->color }}</td>
+									<td>{{ $item->size }}</td>
+									<td>{{ $item->quantity }}</td>
+								</tr>
+							@endforeach
+						@else
+							<tr><td colspan="4">All Items are above 10 Pax</td></tr>
+						@endif
+
+						
 				</table>
 			</div>
 		</div>
-	</div>
-	<div class="col-md-6">
-
 	</div>
 </div>
 
